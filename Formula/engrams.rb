efd12b6620cb1928b8f3cff06842b6c9ac353f4f
Steve Brownlee <chortlehoort@gmail.com>
2026-08-08 13:31:54 -0400

    release: bump Formula to v0.9.0 with verified sha256 hashes

--- Formula/engrams.rb
+++ Formula/engrams.rb
@@ -1,23 +1,23 @@
 class Engrams < Formula
   desc "Standalone, high-performance Rust CLI for managing contextual memory"
   homepage "https://github.com/stevebrownlee/engrams-cli"
-  version "0.8.1"
+  version "0.9.0"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/stevebrownlee/engrams-cli/releases/download/v#{version}/engrams-x86_64-apple-darwin.tar.gz"
-      sha256 "737a229c17a264d7b2c2db9333f6cd0359f6e40f91c6f34efcc9f84065000f84"
+      sha256 "00c4a0757159d3b098a8c4882046d359ddffb581b5596788af9b1c2120698a24"
     elsif Hardware::CPU.arm?
       url "https://github.com/stevebrownlee/engrams-cli/releases/download/v#{version}/engrams-aarch64-apple-darwin.tar.gz"
-      sha256 "607ba01c33b5d73ddc833cfd99bbafd8d52564573c75724207d2486b9039b241"
+      sha256 "73217bda59d36bdabb90dab5527387246bc465208676b487379cb5cf54bc5622"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/stevebrownlee/engrams-cli/releases/download/v#{version}/engrams-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "236151795159996ec0e8348e67aa78ab99a44da7a4e5842272155e69b3a3db9a"
+      sha256 "d604bd58f5826f2d4e1063c43cd95055ea4ecb08469c6e104207c895c26cfb49"
     elsif Hardware::CPU.arm?
       url "https://github.com/stevebrownlee/engrams-cli/releases/download/v#{version}/engrams-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "5bd895561770248d41c9a298e0823d0dcaa5384de7f4cad33ce8c2e6e4fbb14a"
+      sha256 "10e79a479657b9c010b1bd0f3977ee9453b69d755c6aede25331f4f22c34acc1"
     end
   end
 
