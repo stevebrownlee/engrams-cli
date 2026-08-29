189224a51eb6c3505afccbda540b18b763a2a680
Steve Brownlee <chortlehoort@gmail.com>
2026-08-29 17:50:39 -0400

    chore: update Homebrew Formula with verified sha256 checksums for v0.12.0

--- Formula/engrams.rb
+++ Formula/engrams.rb
@@ -6,18 +6,18 @@ class Engrams < Formula
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/stevebrownlee/engrams-cli/releases/download/v#{version}/engrams-x86_64-apple-darwin.tar.gz"
-      sha256 "a1111f55225ed104bc11708ca01bf1b64ffa0ad36702c8732972f404e870b209"
+      sha256 "46931f07573c14c199a73089aff5c8abe8ffd87b55a8cb27d53458c600678088"
     elsif Hardware::CPU.arm?
       url "https://github.com/stevebrownlee/engrams-cli/releases/download/v#{version}/engrams-aarch64-apple-darwin.tar.gz"
-      sha256 "b8bc2b2ff063cee24d9b2c3c550642f40793d784b50c8e7382eac389b5215579"
+      sha256 "f9c0772f90390e0fde92c9e35a0bb8edd848d925f24e7bc3f86eca540f0ce1ee"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/stevebrownlee/engrams-cli/releases/download/v#{version}/engrams-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "0751a4de7377e61042107d08c9646c4672e81e8a87ce1768e504bda0f321614b"
+      sha256 "f00abd9bd0b09c9e51452bb30bf49eee58692d544b56ae524efb8522eb692819"
     elsif Hardware::CPU.arm?
       url "https://github.com/stevebrownlee/engrams-cli/releases/download/v#{version}/engrams-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "5943ced79d3703e5af9fa98bc16b80ce65f2996a2b387543b4e43324a11a6a88"
+      sha256 "dab77e7a57eb6fc1dd43dc9ed2f17e1443d3c91342e8d0820649d86901cdd399"
     end
   end
 
